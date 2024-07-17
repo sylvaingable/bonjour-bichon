c35abcba09b6fdd330111f09177949f395e520bf
Sylvain Gable <sylvain@vingtcinq.io>
2024-07-17 17:55:44 +0200

    Fix failure when sent pictures history file doesn't exist

--- src/sent_pictures.py
+++ src/sent_pictures.py
@@ -1,20 +1,33 @@
 from __future__ import annotations
 
-from typing import Iterable
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Generator, Iterable
 
 from src import config
 
 
+@contextmanager
+def ensure_file_exists() -> Generator[None, None, None]:
+    path = Path(config.SENT_PICTURES_PATH)
+    if not path.exists():
+        path.touch()
+    yield
+
+
+@ensure_file_exists()
 def ls() -> tuple[str, ...]:
     with open(config.SENT_PICTURES_PATH, "r") as f:
         return tuple(f.read().splitlines())
 
 
+@ensure_file_exists()
 def append(paths: Iterable[str]) -> None:
     with open(config.SENT_PICTURES_PATH, "a+") as f:
         f.writelines(path + "\n" for path in paths)
 
 
+@ensure_file_exists()
 def truncate(keep_last_lines: int = 0) -> None:
     lines_to_keep = ls()[-keep_last_lines:]
     with open(config.SENT_PICTURES_PATH, "w") as f:
