4830501eed5f4e3ad247da0c143659acbbb47374
Sylvain Gable <sylvain@vingtcinq.io>
2024-07-17 17:10:17 +0200

    Use simple print statements instead of a logger

--- src/main.py
+++ src/main.py
@@ -1,26 +1,22 @@
-import logging
-
 from . import config, nextcloud_client, pictures_chooser, sent_pictures, signal_client
-
-logger = logging.getLogger(__name__)
 
 
 def send_random_pictures():
-    logger.info("Listing available pictures")
+    print("Listing available pictures")
     pictures_paths = nextcloud_client.list_pictures()
-    logger.info("Listing sent pictures")
+    print("Listing sent pictures")
     sent_pictures_paths = sent_pictures.ls()
-    logger.info("Choosing %s pictures", config.PICTURES_PER_DAY_COUNT)
+    print(f"Choosing {config.PICTURES_PER_DAY_COUNT} pictures")
     chosen_paths = pictures_chooser.choose(
         sent_pictures=sent_pictures_paths, all_pictures=pictures_paths
     )
-    logger.info("Retrieving pictures binary content")
+    print("Retrieving pictures binary content")
     pictures = tuple(nextcloud_client.read_picture(path) for path in chosen_paths)
-    logger.info("Sending pictures")
+    print("Sending pictures")
     signal_client.send_message(text=config.PICTURES_MESSAGE, pictures=pictures)
-    logger.info("Persisting sent pictures")
+    print("Persisting sent pictures")
     sent_pictures.append(chosen_paths)
-    logger.info("Truncating sent pictures journal")
+    print("Truncating sent pictures journal")
     sent_pictures.truncate(
         keep_last_lines=config.PICTURES_PER_DAY_COUNT * config.HISTORY_DAYS_COUNT
     )
